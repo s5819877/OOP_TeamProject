7c739505b343866bb6b631a10aa3662647a67086
s5819877 <5819877@stu.kmu.ac.kr>
2026-05-27 16:37:55 +0900

    초기 커밋.

---
+++ OOP_TeamProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_TeamProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
